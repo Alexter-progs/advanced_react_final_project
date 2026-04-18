dceed5d396a6142d60799dec23d445b023000ac3
Alexter-progs <alexplotbox@gmail.com>
2026-04-18 15:06:48 +0300

    remove weired sort. it shouldn't be on every page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,6 @@ import './styles/normalize.css';
 import './styles/styles.css';
 import { Outlet } from 'react-router-dom';
 import { Header } from '../widgets/Header';
-import { Sort } from '../shared/ui/Sort';
 import { Footer } from '../widgets/Footer';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -11,7 +10,6 @@ export const App = () => {
 	return (
 		<>
 			<Header />
-			<Sort />
 			<Outlet />
 			<ToastContainer
 				position='top-right'
